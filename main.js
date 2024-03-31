06c6bd4c9b96207474af013887462f4835ff7987
Muhammad Saad <148006898+ssaadd-bot@users.noreply.github.com>
2024-04-01 00:15:16 +0500

    To-Do List

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let dueDate = [];
+let time = [];
+let condition = true;
+while (condition) {
+    let todosList = await inquirer.prompt([
+        {
+            name: "todos",
+            type: "input",
+            message: "What you want to add in your To-Do List?",
+        },
+        {
+            name: "dueDate",
+            type: "input",
+            message: "Enter due date for your task (MM/DD/YY)",
+        },
+        {
+            name: "time",
+            type: "input",
+            message: "Enter Time to complete your task (24-Hours-Format(hrs:sec)) ",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to Add more ?",
+            default: "false",
+        },
+    ]);
+    todos.push(todosList.todos);
+    dueDate.push(todosList.dueDate);
+    time.push(todosList.time);
+    condition = todosList.addMore;
+    console.log(todos);
+    console.log(dueDate);
+    console.log(time);
+}
